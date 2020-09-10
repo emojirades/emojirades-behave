01db58d793a31c18d6ff05e21cb635765e9f8b48
Sufendy <sufendy.sufendy@rea-group.com>
2020-09-10 15:01:40 +1000

    Run behave in its own channel

--- features/dummy_chat.feature
+++ features/dummy_chat.feature
@@ -3,4 +3,4 @@ Feature: Dummy Chats
   2 dummies talking to each other
 
   Scenario: Knock Knock
-    When Robbins says "knock knock"
+    When Robbins says "<@U8MLS5TRC> new game <@UNN6M51PB> <@UNWEAHW5D>"
--- features/steps/test_dummy_chat.py
+++ features/steps/test_dummy_chat.py
@@ -3,9 +3,9 @@ from behave import when, then
 
 @when('Robbins says "{phrase}"')
 def robbins_says(context, phrase):
-    context.robbins_bot.send_message(phrase, '#random')
+    context.robbins_bot.send_message(phrase, '#bdd')
 
 
 @then('Dave says "{phrase}"')
 def dave_says(context, phrase):
-    context.dave_bot.send_message(phrase, '#random')
+    context.dave_bot.send_message(phrase, '#bdd')
